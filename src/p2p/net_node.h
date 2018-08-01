bdc152e05ef4ac70f005d6fceced988a2aec3a34
ctilluma <christian@tilluma.com>
2018-08-01 16:16:33 -0500

    Update net_node.h

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -127,10 +127,10 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.moneroseeds.se"
-    , "seeds.moneroseeds.ae.org"
-    , "seeds.moneroseeds.ch"
-    , "seeds.moneroseeds.li"
+    { "coin-01.ins.pa"
+    , "coin-02.ins.pa"
+    , "coin-03.ins.pa"
+    , "coin-04.ins.pa"
     };
 
     bool islimitup=false;
